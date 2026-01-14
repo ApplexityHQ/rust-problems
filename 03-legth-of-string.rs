7c2ae8aeba882a5c118f52b9ca2b2cb852ddb8f9
Applexity ✘🐧 <114644299+ApplexityHQ@users.noreply.github.com>
2026-01-14 22:58:43 +0530

    Create 03-legth-of-string.rs

---
+++ 03-legth-of-string.rs
@@ -0,0 +1,9 @@
+// Que: Write a function get_string_length that takes a string as an input returns its length.
+fn main() {
+    let name = String::from("my name is applexity");
+    let len = get_string_length(name);
+    println!("The length of the string is {}", len);
+}
+fn get_string_length(str: String) -> usize {
+    str.chars().count()
+}
